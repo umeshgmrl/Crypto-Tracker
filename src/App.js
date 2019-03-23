3b644e2f171822ce7d572cc0f3bed6ff4dd7ff80
Bhumesh Domala <umeshgmrl@gmail.com>
2019-03-24 00:56:30 +0530

    add styling

--- src/App.js
+++ src/App.js
@@ -45,74 +45,102 @@ class App extends Component {
     );
   };
 
+  startLoading = () => {
+    this.setState({
+      loading: true
+    });
+  };
+
+  stopLoading = () => {
+    this.setState({
+      loading: false
+    });
+  };
+
   fetchCurrencies = () => {
+    this.startLoading();
     const { page, perPage } = this.state;
+
     fetch(
       `https://api.udilia.com/coins/v1/cryptocurrencies?page=${page}&perPage=${perPage}`
     )
       .then(rawData => rawData.json())
       .then(data => {
         this.setState(data);
+        this.stopLoading();
       });
   };
   componentDidMount() {
     this.fetchCurrencies();
   }
   render() {
-    const { currencies, page, totalPages } = this.state;
-    if (!currencies) return <h2>Loading...</h2>;
+    const { currencies, page, totalPages, loading } = this.state;
+    if (!currencies) return <div className="loader" />;
     return (
       <div className="container">
         <header>
           <h1>Crypto Tracker</h1>
         </header>
-        <footer>
-          <button
-            className="button"
-            disabled={page < 2}
-            onClick={this.decrementPage}
-          >
-            ←
-          </button>
-          <span>{`Page ${page} of ${totalPages}`}</span>
-          <button
-            className="button"
-            disabled={page > 4}
-            onClick={this.incrementPage}
-          >
-            →
-          </button>
+        {loading && <div className="loader" />}
+        {!loading && (
           <div>
-            <span>Per Page</span>
-            <select onChange={this.changePerPage}>
-              <option value="10">10</option>
-              <option value="20">20</option>
-              <option value="50">50</option>
-            </select>
+            <main>
+              <table className="table is-bordered is-fullwidth">
+                <thead>
+                  <tr>
+                    <td>Cryptocurrency</td>
+                    <td>Price</td>
+                    <td>Market Cap</td>
+                    <td>24H Change</td>
+                  </tr>
+                </thead>
+
+                <tbody>
+                  {currencies.map((currency, id) => (
+                    <tr key={id}>
+                      <td>{currency.name}</td>
+                      <td>{currency.price}</td>
+                      <td>{currency.marketCap}</td>
+                      <td>{currency.percentChange24h}</td>
+                    </tr>
+                  ))}
+                </tbody>
+              </table>
+            </main>
+            <footer>
+              <div className="pagination">
+                <button
+                  className="button"
+                  disabled={page < 2}
+                  onClick={this.decrementPage}
+                >
+                  ←
+                </button>
+                <span>{`Page ${page} of ${totalPages}`}</span>
+                <button
+                  className="button"
+                  disabled={page > 4}
+                  onClick={this.incrementPage}
+                >
+                  →
+                </button>
+              </div>
+              <div className="per-page">
+                <span>Per Page</span>
+                <div className="select">
+                  <select
+                    onChange={this.changePerPage}
+                    value={this.state.perPage}
+                  >
+                    <option value="10">10</option>
+                    <option value="20">20</option>
+                    <option value="50">50</option>
+                  </select>
+                </div>
+              </div>
+            </footer>
           </div>
-        </footer>
-        <main>
-          <table className="table is-bordered is-fullwidth">
-            <thead>
-              <tr>
-                <td>Cryptocurrency</td>
-                <td>Price</td>
-                <td>Market Cap</td>
-                <td>24H Change</td>
-              </tr>
-            </thead>
-            <tbody>
-              {currencies.map((currency, id) => (
-                <tr key={id}>
-                  <td>{currency.name}</td>
-                  <td>{currency.price}</td>
-                  <td>{currency.marketCap}</td>
-                  <td>{currency.percentChange24h}</td>
-                </tr>
-              ))}
-            </tbody>
-          </table>
-        </main>
+        )}
       </div>
     );
   }
